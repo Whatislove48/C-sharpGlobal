46cc6642696dcdfd8df8486ed5977cab842e91e3
Vladislav Igorevich <cool-smit3@yandex.ru>
2022-05-01 15:52:50 +0300

    Congrad

--- HomeWork03_04/Lesson04HomeWork/Quest03/Program.cs
+++ HomeWork03_04/Lesson04HomeWork/Quest03/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу,
+//  которая задаёт массив из 8 
+//  элементов и выводит их на экран.
+
+string PrintArr(int[] array) // выводим массив
+{
+    int i = 0;
+    int len = array.Length;
+    string strarr = String.Empty;
+    while (i<len){
+        strarr += array[i] + " ";
+        i+=1;
+        }
+    return strarr;
+}
+
+int[] createArray(int elements) // задаем массив
+{
+    int[] arr = new int [elements];
+    int i = 0;
+    while (i<arr.Length){
+        arr[i] = new Random().Next(0,10);
+        i++;
+    }
+    return arr;
+}
+
+int n = 6; // колличество элементов массива.
+Console.WriteLine(PrintArr(createArray(n)));
