2f2df1842562f8c2c6b3b6b25a8b969c827e8ce9
Vladislav Igorevich <cool-smit3@yandex.ru>
2022-05-01 16:45:14 +0300

    CreateQuest_2

--- HomeWork03_04/Lesson05HomeWork/Quest_2/Program.cs
+++ HomeWork03_04/Lesson05HomeWork/Quest_2/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте одномерный массив,
+// заполненный случайными числами.
+//  Найдите сумму элементов, стоящих 
+//  на нечётных позициях.
+
+int SumOfUneven(int[] numbers)  //Возвращает сумму элементов, 
+{                               // находящихся на нечетных позициях(с нечетн индексом)
+    int result = 0;
+    for (int i = 0; i < numbers.Length; i++)
+        if (i % 2 != 0) result += numbers[i];
+    return result;
+}
+
+int[] createArray(int elements){
+    int[] arr = new int [elements];
+    int i = 0;
+    while (i<arr.Length){
+        arr[i] = new Random().Next(100,1000);
+        i++;
+    }
+    return arr;
+}
+
+string PrintArray(int[] givearr){
+    string result = String.Empty;
+    for (int i = 0; i < givearr.Length; i++)
+    {
+        result += givearr[i] + " | ";
+    }
+    return result;
+}
+
+
+int[] test = createArray(5);
+Console.WriteLine(PrintArray(test));
+Console.WriteLine("Сумма элементов стоящих на нечетных позициях = " + SumOfUneven(test));
+
+
+
+
+
