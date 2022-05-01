6bc167d49d79c959c8639a8dcba9c489015e56fe
Vladislav Igorevich <cool-smit3@yandex.ru>
2022-05-01 16:35:19 +0300

    CreateQuest_1

--- HomeWork03_04/Lesson05HomeWork/Quest_1/Program.cs
+++ HomeWork03_04/Lesson05HomeWork/Quest_1/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿// Задача 1
+// Задайте массив заполненный случайными
+//  положительными трёхзначными числами.
+//   Напишите программу, которая покажет
+//    количество чётных чисел в массиве.
+
+int[] createArray(int elements){
+    int[] arr = new int [elements];
+    int i = 0;
+    while (i<arr.Length){
+        arr[i] = new Random().Next(100,1000);
+        i++;
+    }
+    return arr;
+}
+
+string PrintArray(int[] givearr){
+    string result = String.Empty;
+    for (int i = 0; i < givearr.Length; i++)
+    {
+        result += givearr[i] + " | ";
+    }
+    return result;
+}
+
+int EvenNum(int[] array)
+{
+    int length = array.Length;
+    int result = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] % 2 == 0) result += 1;
+    }
+    return result;
+}
+
+int [] test = createArray(6);
+Console.WriteLine(PrintArray(test));
+Console.WriteLine("Колличество четных = " + EvenNum(test));
