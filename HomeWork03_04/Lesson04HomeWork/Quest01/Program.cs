18f5abba2355c21ebf484a7e399e13280f59a38c
Vladislav Igorevich <cool-smit3@yandex.ru>
2022-05-01 15:06:33 +0300

    AddedQuest01

--- HomeWork03_04/Lesson04HomeWork/Quest01/Program.cs
+++ HomeWork03_04/Lesson04HomeWork/Quest01/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите цикл, который принимает на вход
+//  два числа (A и B) и возводит 
+//  число A в натуральную степень B.
+
+int inPower(int a, int x) // число , степень
+{
+    int res = a;
+    if (x == 0) return 1;
+    for (int i = 1;i<x;i++){
+        res *= a;
+    }
+    return res;
+}
+
+int UserNum = 2;
+int UserPow = 8;
+Console.WriteLine(inPower(UserNum,UserPow));
