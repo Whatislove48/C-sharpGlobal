031db82c39b1c759beea2bbea4d740bde08ccf1d
Vladislav Igorevich <cool-smit3@yandex.ru>
2022-05-01 15:24:38 +0300

    createQuest02

--- HomeWork03_04/Lesson04HomeWork/Quest02/Program.cs
+++ HomeWork03_04/Lesson04HomeWork/Quest02/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая 
+// принимает на вход число и 
+// выдаёт сумму цифр в числе.
+
+
+int SumOfCount(int numeric){
+    int Countnumber = 0; // колличество цифр в числе
+    int x = numeric; // копия числа
+    while (x !=0 ){
+        x /=10;        // отсеивание последней цифры 
+        Countnumber++; // подсчет 
+    }
+    int sum = 0; // сумма
+    int i = Countnumber;   // корректировка индекса
+    while (i>0){
+        sum += (numeric%10);  // суммирование из остатка
+        numeric /= 10;             // срез последней цифры
+        i--;                   
+    } 
+    return sum;            // возвращение 3й цифры
+}
+
+
+int UserNum = 1522;
+
+Console.WriteLine(SumOfCount(UserNum));
+
+
+
