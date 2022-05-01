f05c82e5980ef8f12fe3bac5fa646fb356d341c0
Vladislav Igorevich <cool-smit3@yandex.ru>
2022-05-01 17:04:56 +0300

    CreateQuest_3

--- HomeWork03_04/Lesson05HomeWork/Quest_3/Program.cs
+++ HomeWork03_04/Lesson05HomeWork/Quest_3/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача 3: Задайте массив вещественных чисел.
+//  Найдите разницу между максимальным 
+//  и минимальным элементов массива.
+
+string PrintArray(double[] givearr){
+    string result = String.Empty;
+    for (int i = 0; i < givearr.Length; i++)
+    {
+        result += givearr[i] + " | ";
+    }
+    return result;
+}
+
+
+
+double[] createArray(int elements){
+    double[] arr = new double [elements];
+    int i = 0;
+    while (i<arr.Length){
+        arr[i] = Math.Round(new Random().NextDouble()*100,2);
+        i++;
+    }
+    return arr;
+}
+
+double MaxMinDif(double[] arr){
+    double max = arr[0];
+    double min = arr[0];
+    for (int i = 0; i< arr.Length;i++){
+        if (max < arr[i]){
+            max = arr[i];
+        }
+        if (min >= arr[i]){
+            min = arr[i];
+        }
+    }
+    return Math.Round(max - min,2);
+}
+
+
+double[] UserArr = createArray(6);
+Console.WriteLine(PrintArray(UserArr));
+Console.WriteLine(MaxMinDif(UserArr));
+
+
